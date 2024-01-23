8a7cabdac05d29ab645148887d217cd66fbba90e
casscannelle <cassia.maffei@gmail.com>
2024-01-23 17:35:50 -0300

    ajuste endereço calculator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{ useState, useEffect } from 'react';
 import './App.css'
-import Calculator from './components/Calculator/Calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   
